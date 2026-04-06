168985c9d4ced8b33fad6ea3bbc0d47959d7a9a8
wiktor <wiktor.kycia@gmail.com>
2026-04-06 13:35:06 +0200

    add the isLoading and isError logic in frontend

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,11 +2,22 @@ import {useGetAPI} from "../../hooks/useGetAPI.ts";
 
 export default function Home()
 {
-    const {data} = useGetAPI('http://localhost:3000/')
+    const {data, isLoading, isError} = useGetAPI('http://localhost:3000/')
 
     console.log(data)
 
     return (
-        <div>{data.content}</div>
+        <>
+            {isLoading && (
+                <div>Loading</div>
+            )}
+            {isError && (
+                <div>Error</div>
+            )}
+            {!isLoading && !isError && data && (
+                <div>{data.content}</div>
+            )}
+        </>
+
     )
 }
