8dc13524a23e9372082f452eda2a197fb0d3b64b
wiktor <wiktor.kycia@gmail.com>
2026-04-06 13:25:52 +0200

    Revert "add proxy to the vite config on frontend"
    This reverts commit 9db4470c29931ccdda680778a23e7cd3383fbb1f.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        plugins: [react()],
-        server: {
-            watch: {
-                usePolling: true,
-            },
-            host: '0.0.0.0',
-            port: 5173,
-            proxy: {
-                '/': {
-                    target: env.VITE_API_TARGET || 'http://app:3000',
-                    changeOrigin: true
-                }
-            }
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
         },
-    }
+        host: '0.0.0.0',
+        port: 5173,
+    },
 })
