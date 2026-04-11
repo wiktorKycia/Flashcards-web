f3d6922e6f4a1db4e2d9b36cb5b7c64555c055fe
wiktor <wiktor.kycia@gmail.com>
2026-04-11 22:30:39 +0200

    apply the class in the Quiz module and add some module css classes, so the elements are placed correctly

--- frontend/src/pages/Quiz/Quiz.tsx
+++ frontend/src/pages/Quiz/Quiz.tsx
@@ -5,31 +5,35 @@ import AttachedFlashcardsMode from "../../components/AttachedFlashcardsMode";
 import EditableFlashcard from "../../components/EditableFlashcard";
 import ButtonTop from "../../components/ButtonTop";
 import ToolBar from "../../components/ToolBar";
+import styles from './Quiz.module.scss'
 
 export default function Quiz() {
     return (
         <>
             <Header/>
-            <main>
+            <main className={styles.Main}>
                 <ToolBar/>
-                <div>
-                    <Container>
-                        <h1>Nazwa quizu</h1>
-                        <button>save</button>
-                        <button>share</button>
-                        <button>more options</button>
+                <div className={styles.MainRight}>
+                    <h1>Nazwa quizu</h1>
+                    <Container cssClassName={"container-vertical-borderless"}>
+                        <button>zapisz</button>
+                        <button>udostępnij</button>
+                        <button>edytuj</button>
+                        <button>kopiuj</button>
+                        <button>eksport do pliku</button>
+                        <button>usuń</button>
                         {/* tutaj opcje: edytuj (twórca), kopiuj (zalogowany), eksport (wszyscy), usuń (twórca) */}
                     </Container>
-                    <Container>
+                    <Container cssClassName={"container-vertical-borderless"}>
                         <button>fiszki</button>
-                        <button>ucz sie</button>
+                        <button>ucz się  </button>
                         <button>test</button>
                         <button>dopasowania</button>
                     </Container>
                     <Container>
                         <AttachedFlashcardsMode/>
                     </Container>
-                    <Container>
+                    <Container cssClassName={"container-vertical-borderless"}>
                         <Person name={"John doe"} title={"author"}/>
                         <button>like</button>
                         <button>dislike</button>
