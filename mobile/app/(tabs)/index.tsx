9bb538940f087d50f6aafe167531ce6196c831de
wiktor <wiktor.kycia@gmail.com>
2026-04-14 22:21:56 +0200

    comment out useless things

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -108,56 +108,56 @@ export default function HomeScreen() {
                     <ThemedText>{apiMessage}</ThemedText>
                 )}
             </ThemedView>
-            <ThemedView style={styles.stepContainer}>
-                <Link href="/modal">
-                    <Link.Trigger>
-                        <ThemedText type="subtitle">
-                            Step 67: Explore
-                        </ThemedText>
-                    </Link.Trigger>
-                    <Link.Preview />
-                    <Link.Menu>
-                        <Link.MenuAction
-                            title="Action"
-                            icon="cube"
-                            onPress={() => alert('Action pressed')}
-                        />
-                        <Link.MenuAction
-                            title="Share"
-                            icon="square.and.arrow.up"
-                            onPress={() => alert('Share pressed')}
-                        />
-                        <Link.Menu title="More" icon="ellipsis">
-                            <Link.MenuAction
-                                title="Delete"
-                                icon="trash"
-                                destructive
-                                onPress={() => alert('Delete pressed')}
-                            />
-                        </Link.Menu>
-                    </Link.Menu>
-                </Link>
+            {/*<ThemedView style={styles.stepContainer}>*/}
+            {/*    <Link href="/modal">*/}
+            {/*        <Link.Trigger>*/}
+            {/*            <ThemedText type="subtitle">*/}
+            {/*                Step 67: Explore*/}
+            {/*            </ThemedText>*/}
+            {/*        </Link.Trigger>*/}
+            {/*        <Link.Preview />*/}
+            {/*        <Link.Menu>*/}
+            {/*            <Link.MenuAction*/}
+            {/*                title="Action"*/}
+            {/*                icon="cube"*/}
+            {/*                onPress={() => alert('Action pressed')}*/}
+            {/*            />*/}
+            {/*            <Link.MenuAction*/}
+            {/*                title="Share"*/}
+            {/*                icon="square.and.arrow.up"*/}
+            {/*                onPress={() => alert('Share pressed')}*/}
+            {/*            />*/}
+            {/*            <Link.Menu title="More" icon="ellipsis">*/}
+            {/*                <Link.MenuAction*/}
+            {/*                    title="Delete"*/}
+            {/*                    icon="trash"*/}
+            {/*                    destructive*/}
+            {/*                    onPress={() => alert('Delete pressed')}*/}
+            {/*                />*/}
+            {/*            </Link.Menu>*/}
+            {/*        </Link.Menu>*/}
+            {/*    </Link>*/}
 
-                <ThemedText>
-                    {`Tap the Explore tab to learn more about what's included in this starter app.`}
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">
-                    Step 3: Get a fresh start
-                </ThemedText>
-                <ThemedText>
-                    {`When you're ready, run `}
-                    <ThemedText type="defaultSemiBold">
-                        npm run reset-project
-                    </ThemedText>{' '}
-                    to get a fresh{' '}
-                    <ThemedText type="defaultSemiBold">app</ThemedText>{' '}
-                    directory. This will move the current{' '}
-                    <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-                    <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-                </ThemedText>
-            </ThemedView>
+            {/*    <ThemedText>*/}
+            {/*        {`Tap the Explore tab to learn more about what's included in this starter app.`}*/}
+            {/*    </ThemedText>*/}
+            {/*</ThemedView>*/}
+            {/*<ThemedView style={styles.stepContainer}>*/}
+            {/*    <ThemedText type="subtitle">*/}
+            {/*        Step 3: Get a fresh start*/}
+            {/*    </ThemedText>*/}
+            {/*    <ThemedText>*/}
+            {/*        {`When you're ready, run `}*/}
+            {/*        <ThemedText type="defaultSemiBold">*/}
+            {/*            npm run reset-project*/}
+            {/*        </ThemedText>{' '}*/}
+            {/*        to get a fresh{' '}*/}
+            {/*        <ThemedText type="defaultSemiBold">app</ThemedText>{' '}*/}
+            {/*        directory. This will move the current{' '}*/}
+            {/*        <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}*/}
+            {/*        <ThemedText type="defaultSemiBold">app-example</ThemedText>.*/}
+            {/*    </ThemedText>*/}
+            {/*</ThemedView>*/}
         </ParallaxScrollView>
     )
 }
