e0982566c224e0feed851f04c0754ff184d7eea7
wiktor <wiktor.kycia@gmail.com>
2026-04-15 10:47:41 +0200

    handle the shuffle button

--- frontend/src/components/AttachedFlashcardsMode/AttachedFlashcardsMode.tsx
+++ frontend/src/components/AttachedFlashcardsMode/AttachedFlashcardsMode.tsx
@@ -10,23 +10,30 @@ export default function AttachedFlashcardsMode(props: AttachedFlashcardsModeProp
     const [isTrackingProgress, setIsCheckingProgress] = useState<boolean>(false);
     const [flashcardsIterator, setFlashcardsIterator] = useState<number>(0);
 
-
+    const [flashcards, setFlashcards] = useState<Array<Flashcard>>(props.flashcards)
 
     function handleShuffle() {
-
+        const array = [...flashcards];
+        for (let i = array.length -1; i > 0; i--)
+        {
+            const j = Math.floor(Math.random() * (i+1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        setFlashcards(array);
+        setFlashcardsIterator(0)
     }
 
     return (
         <>
             <div>
-                <BigFlashcard front={props.flashcards[flashcardsIterator].front} back={props.flashcards[flashcardsIterator].back}/> {/* oddzielny komponent na wielką fiszkę */}
+                <BigFlashcard front={flashcards[flashcardsIterator].front} back={flashcards[flashcardsIterator].back}/> {/* oddzielny komponent na wielką fiszkę */}
             </div>
             <div>
                 <label htmlFor="track_progress">Śledź postępy</label> {/*tylko dla użytkowników zalogowanych*/}
                 <input type="checkbox" name="track_progress" id="track_progress" checked={isTrackingProgress} onClick={() => setIsCheckingProgress((prevState) => !prevState)}/>
 
                 <button onClick={() => setFlashcardsIterator((prevState) => prevState+1)}>←</button>
-                <div>{flashcardsIterator+1} / {props.flashcards.length}</div>
+                <div>{flashcardsIterator+1} / {flashcards.length}</div>
                 <button onClick={() => setFlashcardsIterator((prevState) => prevState-1)}>→</button>
 
                 {isTrackingProgress && (
