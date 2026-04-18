fa7ae1853c4e02632b35e44089df4e8240318021
wiktor <wiktor.kycia@gmail.com>
2026-04-18 22:01:21 +0200

    use the login mutation

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,15 +1,27 @@
 import {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useAuth} from '@/context/AuthContext'
+import { useLogin } from '@/hooks/useLogin.ts'
 
 export default function Login() {
     const [form, setForm] = useState({login: '', password: ''})
     const {login} = useAuth()
     const navigate = useNavigate()
+
+    const loginMutation = useLogin()
     
     const handleSubmit = async (e: { preventDefault: () => void }) => {
         e.preventDefault()
-
+        loginMutation.mutate({
+            login: form.login,
+            password: form.password
+        }, {
+            onSuccess: (data) => {
+                console.log(data)
+                login(data.token)
+                navigate('/')
+            }
+        })
     }
     
     return (
