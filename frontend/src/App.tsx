6f88bb9b38315a8161cdd4bf3c6742b93327e1ff
wiktor <wiktor.kycia@gmail.com>
2026-04-19 13:15:00 +0200

    add Login and Register pages to the router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import UserProfile from './pages/UserProfile'
 import { Routes, Route } from 'react-router'
 import Quiz from "./pages/Quiz";
 import { AuthProvider } from '@/context/AuthContext.tsx'
+import Register from '@/pages/Register/Register.tsx'
+import Login from '@/pages/Login/Login.tsx'
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/profile/' element={<UserProfile/>}/>
                 <Route path='/quiz/' element={<Quiz quizName={"test"}/>}/>
+                <Route path='/register/' element={<Register/>}/>
+                <Route path='/login/' element={<Login/>}/>
             </Routes>
         </AuthProvider>
 
