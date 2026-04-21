1f96843dc894ef1ff3f499bcb1a5041ceeb90686
wiktor <wiktor.kycia@gmail.com>
2026-04-21 19:20:19 +0200

    handle tanstack query in Quiz component

--- frontend/src/pages/Quiz/Quiz.tsx
+++ frontend/src/pages/Quiz/Quiz.tsx
@@ -9,6 +9,7 @@ import {useParams} from "react-router";
 import {useGetAPI} from '@/hooks/useGetAPI.ts';
 import ListedFlashcards from '@/components/ListedFlashcards'
 import { useAuth } from '@/context/AuthContext.tsx'
+import LoadingSpinner from '@/components/LoadingSpinner'
 
 export default function Quiz() {
     const id: number = parseInt(useParams().id as string)
@@ -28,54 +29,62 @@ export default function Quiz() {
             <Header />
             <main className={styles.Main}>
                 <ToolBar />
-                <div className={styles.MainRight}>
-                    <h1>{data.quizName || 'Nazwa quizu'}</h1>
-                    <Container cssClassName={'container-vertical-borderless'}>
-                        <button>eksport do pliku</button>
-                        <button>udostępnij</button>
+                {isError && (
+                    <div>wystąpił błąd</div>
+                )}
+                {isLoading && (
+                    <LoadingSpinner/>
+                )}
+                {!isLoading && !isError && data && (
+                    <div className={styles.MainRight}>
+                        <h1>{data.quizName || 'Nazwa quizu'}</h1>
+                        <Container cssClassName={'container-vertical-borderless'}>
+                            <button>eksport do pliku</button>
+                            <button>udostępnij</button>
 
-                        {isLoggedIn && (
-                            <>
-                                <button>zapisz</button>
-                                <button>kopiuj</button>
-                            </>)
-                        }
-                        {isUserAuthor && (
-                            <>
-                                <button>edytuj</button>
-                                <button>usuń</button>
-                            </>
-                        )}
-
-                        {/* tutaj opcje: edytuj (twórca), kopiuj (zalogowany), eksport (wszyscy), usuń (twórca) */}
-                    </Container>
-                    <Container cssClassName={'container-vertical-borderless'}>
-                        <button>ucz się</button>
-                        <button>dopasowania</button>
-                    </Container>
-                    <Container>
-                        <AttachedFlashcardsMode />
-                    </Container>
-                    <Container cssClassName={'container-vertical-borderless'}>
-                        <Person name={'John doe'} title={'author'} />
-                        <button>like</button>
-                        <button>dislike</button>
-                    </Container>
-                    <ListedFlashcards
-                        flashcards={[
-                            {
-                                database_id: 1,
-                                front: 'front',
-                                back: 'back',
-                                langFront: 'english',
-                                langBack: 'english',
-                                isStarred: true
+                            {isLoggedIn && (
+                                <>
+                                    <button>zapisz</button>
+                                    <button>kopiuj</button>
+                                </>)
                             }
-                        ]}
-                        isUserLoggedIn={false}
-                        isUserAuthor={false}
-                    />
-                </div>
+                            {isUserAuthor && (
+                                <>
+                                    <button>edytuj</button>
+                                    <button>usuń</button>
+                                </>
+                            )}
+
+                            {/* tutaj opcje: edytuj (twórca), kopiuj (zalogowany), eksport (wszyscy), usuń (twórca) */}
+                        </Container>
+                        <Container cssClassName={'container-vertical-borderless'}>
+                            <button>ucz się</button>
+                            <button>dopasowania</button>
+                        </Container>
+                        <Container>
+                            <AttachedFlashcardsMode />
+                        </Container>
+                        <Container cssClassName={'container-vertical-borderless'}>
+                            <Person name={'John doe'} title={'author'} />
+                            <button>like</button>
+                            <button>dislike</button>
+                        </Container>
+                        <ListedFlashcards
+                            flashcards={[
+                                {
+                                    database_id: 1,
+                                    front: 'front',
+                                    back: 'back',
+                                    langFront: 'english',
+                                    langBack: 'english',
+                                    isStarred: true
+                                }
+                            ]}
+                            isUserLoggedIn={false}
+                            isUserAuthor={false}
+                        />
+                    </div>
+                )}
             </main>
             <ButtonTop />
         </>
