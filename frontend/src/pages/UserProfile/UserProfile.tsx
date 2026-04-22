a07d283aa007332a492a38a79853fb41875875a5
wiktor <wiktor.kycia@gmail.com>
2026-04-22 21:29:03 +0200

    in UserProfile grab the id from params

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,10 +1,12 @@
 import styles from './UserProfile.module.scss'
+import { useParams } from 'react-router'
 // import { useAuth } from '@/context/AuthContext.tsx'
 
 export default function UserProfile() {
     // const isLoggedIn = !!useAuth().token
+    const userId: number = parseInt(useParams().id as string)
 
     return (
-        <div className={styles.UserProfile}>information about the user</div>
+        <div className={styles.UserProfile}>information about the user {userId}</div>
     )
 }
