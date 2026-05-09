8ab61f28c36c133f83c6300ad612329b65ed7812
wiktor <wiktor.kycia@gmail.com>
2026-05-09 18:17:41 +0200

    add QuizCreate page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import Home from './pages/Home'
-import UserProfile from './pages/UserProfile'
 import { Routes, Route } from 'react-router'
-import Quiz from './pages/Quiz'
+import Home from '@/pages/Home'
+import UserProfile from '@/pages/UserProfile'
+import Quiz from '@/pages/Quiz'
 import AuthProvider from '@/context/AuthContext.tsx'
 import Register from '@/pages/Register/Register.tsx'
 import Login from '@/pages/Login/Login.tsx'
 import UserSettings from '@/pages/UserSettings'
 import Header from "@/components/Header";
+import QuizCreate from '@/pages/QuizCreate'
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                 <Route path="/user/" element={<UserSettings />} />
                 <Route path="/user/:id" element={<UserProfile />} />
                 <Route path="/quiz/">
+                    <Route path="/quiz/create" element={<QuizCreate/>}/>
                     <Route path=":id" element={<Quiz />} />
                 </Route>
                 <Route path="/register/" element={<Register />} />
---
+++ frontend/src/pages/QuizCreate/QuizCreate.tsx
@@ -0,0 +1,7 @@
+import styles from './QuizCreate.module.scss'
+
+export default function QuizCreate(){
+    return (
+        <div className={styles.QuizCreate}>quiz create</div>
+    )
+}
