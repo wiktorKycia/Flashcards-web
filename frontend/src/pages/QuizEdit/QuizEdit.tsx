885f58707e842f917e3308b6865b1f3f2400eb40
wiktor <wiktor.kycia@gmail.com>
2026-05-10 17:01:33 +0200

    create QuizEdit page this will be a big form for editing quizzes

---
+++ frontend/src/pages/QuizEdit/QuizEdit.tsx
@@ -0,0 +1,13 @@
+import styles from './QuizEdit.module.scss'
+import ButtonTop from '@/components/ButtonTop'
+
+export default function QuizEdit(){
+    return (
+        <>
+            <main className={styles.QuizCreate}>
+
+            </main>
+            <ButtonTop/>
+        </>
+    )
+}
