52ff09968dcb9276fe3e0e0e24bac95fc54dacb0
wiktor <wiktor.kycia@gmail.com>
2026-05-10 17:51:15 +0200

    make some security protections so that no unauthorized user can access the specific page

--- frontend/src/pages/QuizEdit/QuizEdit.tsx
+++ frontend/src/pages/QuizEdit/QuizEdit.tsx
@@ -1,7 +1,27 @@
 import styles from './QuizEdit.module.scss'
 import ButtonTop from '@/components/ButtonTop'
+import { useLoggedInOnly } from '@/hooks/useLoggedInOnly.ts'
+import { useAuth } from '@/context/AuthContext.tsx'
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router'
+import { useQuizData } from '@/hooks/useQuizData.ts'
 
 export default function QuizEdit(){
+    useLoggedInOnly()
+    const auth = useAuth()
+    const navigate = useNavigate()
+
+    const id: number = parseInt(useParams().id as string)
+    const { data, isLoading, isError, error } = useQuizData(id)
+
+    console.log(data, isLoading, isError, error)
+
+    console.log(auth.user?.id, data?.quiz.authorId)
+    if (auth.user?.id !== data?.quiz.authorId)
+    {
+        navigate(-1) // go back by one page
+    }
+
     return (
         <>
             <main className={styles.QuizCreate}>
