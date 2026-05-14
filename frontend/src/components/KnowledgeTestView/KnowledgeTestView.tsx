10fb98005697c8c9ff680b2e1736f1e4d2520437
JanTopolewski <jankotopolewski@gmail.com>
2026-05-14 02:39:04 +0200

    Add structure for KnowledgeTestView component

--- frontend/src/components/KnowledgeTestView/KnowledgeTestView.tsx
+++ frontend/src/components/KnowledgeTestView/KnowledgeTestView.tsx
@@ -1,7 +1,61 @@
-export default function KnowledgeTestView() {
-    return (
-        <>
+import GapTask from '@/components/GapTask'
+import SingleChoiceTask from '@/components/SingleChoiceTask'
 
-        </>
+type Props = {
+    data: {
+        fillGap?: {
+            data: never[]
+        }
+        firstLetterGap?: {
+            data: never[]
+        }
+        singleChoice?: {
+            data: never[]
+        }
+    }
+}
+
+export default function KnowledgeTestView({ data }: Props) {
+    return (
+        <div>
+            {data.fillGap?.data?.length ? (
+                <section>
+                    <h2>Wypełnij luki</h2>
+
+                    {data.fillGap.data.map((task, i) => (
+                        <GapTask
+                            key={i}
+                            task={task}
+                        />
+                    ))}
+                </section>
+            ) : null}
+
+            {data.firstLetterGap?.data?.length ? (
+                <section>
+                    <h2>Wypełnij pozostałe części fraz w lukach na podstawie ich pierwszych liter</h2>
+
+                    {data.firstLetterGap.data.map((task, i) => (
+                        <GapTask
+                            key={i}
+                            task={task}
+                        />
+                    ))}
+                </section>
+            ) : null}
+
+            {data.singleChoice?.data?.length ? (
+                <section>
+                    <h2>Wybierz poprawne uzupełnienie luki</h2>
+
+                    {data.singleChoice.data.map((task, i) => (
+                        <SingleChoiceTask
+                            key={i}
+                            task={task}
+                        />
+                    ))}
+                </section>
+            ) : null}
+        </div>
     )
 }
