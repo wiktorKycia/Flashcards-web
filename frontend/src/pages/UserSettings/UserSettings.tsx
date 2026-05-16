80a6475772ae7b348530a9b5ebb0d6f06e2aaa24
wiktor <wiktor.kycia@gmail.com>
2026-05-16 16:20:20 +0200

    update the user settings

--- frontend/src/pages/UserSettings/UserSettings.tsx
+++ frontend/src/pages/UserSettings/UserSettings.tsx
@@ -1,22 +1,17 @@
 import styles from './UserSettings.module.scss'
 import { useAuth } from '@/context/AuthContext.tsx'
-import { useNavigate } from 'react-router-dom'
 import Container from '@/components/Container'
 import LoadingSpinner from '@/components/LoadingSpinner'
 import { useSavedQuizzes } from '@/hooks/useSavedQuizzes.ts'
-import { useEffect } from 'react'
 import { Link } from 'react-router'
+import { useLoggedInOnly } from '@/hooks/useLoggedInOnly.ts'
+import FieldGroup from '@/components/FieldGroup'
+import type { ChangeEvent } from 'react'
 
 export default function UserSettings() {
-    const navigate = useNavigate()
-    const auth = useAuth()
+    useLoggedInOnly()
 
-    const isLoggedIn = !!auth.token
-    useEffect(() => {
-        if (!isLoggedIn) {
-            navigate('/login')
-        }
-    }, [isLoggedIn, navigate])
+    const auth = useAuth()
 
     const { data, isLoading, isError } = useSavedQuizzes(
         auth.user?.id as number
@@ -24,16 +19,48 @@ export default function UserSettings() {
     console.log(data)
 
     return (
-        <div className={styles.UserSettings}>
-            <h1>Hello {auth.user?.name}</h1>
+        <main className={styles.UserSettings}>
+            <h1>Ustawienia użytkownika</h1>
+            {auth.user?.name ? (
+                <section className={styles.UserSettingsSection}>
+                    <Container cssClassName={styles.UserSettingsLeft}>
+                        <h2>Dane</h2>
 
-            <div className={styles.ButtonGroup}>
-                <button onClick={auth.logout}>Logout</button>
-                <button>Reset password</button>
-                <button>Edit username</button>
-            </div>
+                        <form>
+                            <FieldGroup
+                                labelText="Nazwa użytkownika"
+                                inputHTMLId="input_username"
+                                inputType="text"
+                                inputValue={auth.user.name}
+                                onInputChange={(event: ChangeEvent) => {
+                                    console.log(event.target)}
+                                }
+                                isVertical={false}
+                            />
 
+                            <FieldGroup
+                                labelText="Email"
+                                inputHTMLId="input_email"
+                                inputType="text"
+                                inputValue={"email"}
+                                onInputChange={(event: ChangeEvent) => {
+                                    console.log(event.target)
+                                }}
+                                isVertical={false}
+                            />
+                        </form>
+                    </Container>
 
+                    <Container cssClassName={styles.UserSettingsRight}>
+                        <h2>Akcje</h2>
+                        <button onClick={auth.logout}>Wyloguj</button>
+                        <button>Resetuj hasło</button>
+                    </Container>
+                </section>
+            ):(
+                <LoadingSpinner />
+            )}
+            {/*Move it to the user profile*/}
             {isError && <p style={{ color: 'var(--color-accent2)' }}>wystąpił błąd</p>}
             {isLoading && <LoadingSpinner />}
             {data && data.map((quiz) => (
@@ -45,6 +72,6 @@ export default function UserSettings() {
                 </Container>
             ))}
 
-        </div>
+        </main>
     )
 }
