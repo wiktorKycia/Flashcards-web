54f6d662ecb25479c712bdbcd78b1b0a59823563
wiktor <wiktor.kycia@gmail.com>
2026-05-17 17:59:09 +0200

    correct the UserProfile

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,11 +1,11 @@
-import { useParams } from 'react-router'
-import { useUserInfo } from '@/hooks/useUserInfo.ts'
-// import Container from '@/components/Container'
+import { Link, useParams } from 'react-router'
 import styles from './UserProfile.module.scss'
 // import { useSavedQuizzes } from '@/hooks/useSavedQuizzes.ts'
 import { useAuth } from '@/context/AuthContext.tsx'
-// import LoadingSpinner from '@/components/LoadingSpinner'
+import LoadingSpinner from '@/components/LoadingSpinner'
 import { useLoggedInOnly } from '@/hooks/useLoggedInOnly'
+import {useUserProfile} from "@/hooks/useUserProfile.ts";
+import Container from "@/components/Container";
 
 
 export default function UserProfile() {
@@ -15,43 +15,43 @@ export default function UserProfile() {
 
     const auth = useAuth()
 
-    const userInfo = useUserInfo(userId)
-    console.log(userInfo);
-    
+    const isVisitingSelf = auth.user ? userId === auth.user.id : false
+    const { data, isLoading, isError } = useUserProfile(userId, isVisitingSelf)
+    console.log(data, isLoading, isError)
 
-    // const { data, isLoading, isError } = useSavedQuizzes(
-    //     auth.user?.id as number
-    // )
+    if (!auth.user)
+    {
+        return <LoadingSpinner/>
+    }
 
     return (
-        <div className={styles.UserProfile}>
-            Hello world from user profile!
-            visiting user id: {auth.user?.id}
-            {/* {data && <h1>{data.name}</h1>}
-            {data &&
-                data.createdQuizzes.map((quiz) => (
-                    <Container key={quiz.id}>
-                        <div className={styles.QuizItem}>
-                            <h2>{quiz.name}</h2>
-                            <p>{quiz.description}</p>
-                            <p>ID: {quiz.id}</p>
-                        </div>
-                    </Container>
-                ))}
-            
-
-
+        <main className={styles.UserProfile}>
             {isError && <p style={{ color: 'var(--color-accent2)' }}>wystąpił błąd</p>}
             {isLoading && <LoadingSpinner />}
-            {data && data.map((quiz) => (
-                <Container key={quiz.id}>
-                    <Link to={`/quiz/${quiz.id}`} className={styles.QuizItem}>
-                        <h2>{quiz.name}</h2>
-                        <p>{quiz.description}</p>
-                    </Link>
-                </Container>
-            ))} */}
 
-        </div>
+            {data && (
+                <>
+                    <h1>{data.name}</h1>
+
+                    {data.createdQuizzes.map((quiz) => (
+                        <Container key={quiz.id}>
+                            <div className={styles.QuizItem}>
+                                <h2>{quiz.name}</h2>
+                                <p>{quiz.description}</p>
+                            </div>
+                        </Container>
+                    ))}
+
+                    {data.savedQuizzes && data.savedQuizzes.map((quiz) => (
+                        <Container key={quiz.id}>
+                            <Link to={`/quiz/${quiz.id}`} className={styles.QuizItem}>
+                                <h2>{quiz.name}</h2>
+                                <p>{quiz.description}</p>
+                            </Link>
+                        </Container>
+                    ))}
+                </>
+            )}
+        </main>
     )
 }
