bd163911cb81a3d177564aba11e4952e7965ab19
wiktor <wiktor.kycia@gmail.com>
2026-05-17 17:58:33 +0200

    optimize UserSettings elements display

--- frontend/src/pages/UserSettings/UserSettings.tsx
+++ frontend/src/pages/UserSettings/UserSettings.tsx
@@ -11,48 +11,49 @@ export default function UserSettings() {
 
     const auth = useAuth()
 
+    if (!auth.user)
+    {
+        return <LoadingSpinner/>
+    }
+
     return (
         <main className={styles.UserSettings}>
             <h1>Ustawienia użytkownika</h1>
-            {auth.user?.name ? (
-                <section className={styles.UserSettingsSection}>
-                    <Container cssClassName={styles.UserSettingsLeft}>
-                        <h2>Dane</h2>
+            <section className={styles.UserSettingsSection}>
+                <Container cssClassName={styles.UserSettingsLeft}>
+                    <h2>Dane</h2>
 
-                        <form>
-                            <FieldGroup
-                                labelText="Nazwa użytkownika"
-                                inputHTMLId="input_username"
-                                inputType="text"
-                                inputValue={auth.user.name}
-                                onInputChange={(event: ChangeEvent) => {
-                                    console.log(event.target)}
-                                }
-                                isVertical={false}
-                            />
+                    <form>
+                        <FieldGroup
+                            labelText="Nazwa użytkownika"
+                            inputHTMLId="input_username"
+                            inputType="text"
+                            inputValue={auth.user.name}
+                            onInputChange={(event: ChangeEvent) => {
+                                console.log(event.target)}
+                            }
+                            isVertical={false}
+                        />
 
-                            <FieldGroup
-                                labelText="Email"
-                                inputHTMLId="input_email"
-                                inputType="text"
-                                inputValue={"email"}
-                                onInputChange={(event: ChangeEvent) => {
-                                    console.log(event.target)
-                                }}
-                                isVertical={false}
-                            />
-                        </form>
-                    </Container>
+                        <FieldGroup
+                            labelText="Email"
+                            inputHTMLId="input_email"
+                            inputType="text"
+                            inputValue={"email"}
+                            onInputChange={(event: ChangeEvent) => {
+                                console.log(event.target)
+                            }}
+                            isVertical={false}
+                        />
+                    </form>
+                </Container>
 
-                    <Container cssClassName={styles.UserSettingsRight}>
-                        <h2>Akcje</h2>
-                        <button onClick={auth.logout}>Wyloguj</button>
-                        <button>Resetuj hasło</button>
-                    </Container>
-                </section>
-            ):(
-                <LoadingSpinner />
-            )}
+                <Container cssClassName={styles.UserSettingsRight}>
+                    <h2>Akcje</h2>
+                    <button onClick={auth.logout}>Wyloguj</button>
+                    <button>Resetuj hasło</button>
+                </Container>
+            </section>
         </main>
     )
 }
